bce3fd409cbd7c27013edc6c3da3410599444c19
zameariislam <zameariislam@gmail.com>
2022-07-26 12:00:25 +0200

    pages are made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import Home from './component/Home';
+
+
 
 function App() {
   return (
     <div >
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route></Route>
+      </Routes>
     
     </div>
   );
